7e120a141e95e0e50c6605086acc2506bffa1b1e
JusethAg <mauagueda@gmail.com>
2021-04-16 05:03:13 -0500

    Refactor unit tests to standarize format following vscode snippets

--- main_test.go
+++ main_test.go
@@ -9,9 +9,9 @@ import (
 
 func TestGetFlags(t *testing.T) {
 	tests := []struct {
-		name           string
-		inputArgs      []string
-		expectedResult flags
+		name      string
+		inputArgs []string
+		want      flags
 	}{
 		{"No flags", []string{"./os-info-cli"}, flags{all: true}},
 		{"Set 'a' flag", []string{"./os-info-cli", "-a"}, flags{all: true}},
@@ -37,10 +37,26 @@ func TestGetFlags(t *testing.T) {
 			// Simulate command line arguments
 			os.Args = tt.inputArgs
 
-			output := GetFlagsFromCommandLine()
+			got := GetFlagsFromCommandLine()
 
-			if !reflect.DeepEqual(tt.expectedResult, output) {
-				t.Errorf("Expected value %v, got %v", tt.expectedResult, output)
+			if !reflect.DeepEqual(tt.want, got) {
+				t.Errorf("TestGetFlags() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetNetworkInfo(t *testing.T) {
+	tests := []struct {
+		name string
+		want networkInfo
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GetNetworkInfo(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetNetworkInfo() = %v, want %v", got, tt.want)
 			}
 		})
 	}
